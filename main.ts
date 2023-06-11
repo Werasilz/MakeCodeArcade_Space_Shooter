f1cfe83b4dbe9ef92d9b0c4e38667dd44fc6e1af
Werasil Rerkweang <zen_frame@hotmail.com>
2023-06-11 13:13:54 +0700

    Player Controller

--- main.ts
+++ main.ts
@@ -1,1 +1,41 @@
-
+/**
+ * Fire Rate Upgrade
+ * 
+ * Level 1 = 500
+ * 
+ * Level 2 = 300
+ * 
+ * Level 3 = 100 (Max)
+ */
+function PlayerInit () {
+    playerSprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 9 9 . . . . . . . 
+        . . . . . . . 9 9 . . . . . . . 
+        . . . . . . 6 9 9 6 . . . . . . 
+        . . . . . 8 6 9 9 6 8 . . . . . 
+        . . . . 8 6 8 6 6 8 6 8 . . . . 
+        . . . . 8 6 8 8 8 8 6 8 . . . . 
+        . . . 8 6 6 8 6 6 8 6 6 8 . . . 
+        . . 6 8 8 8 8 c b 8 8 8 8 6 . . 
+        . 6 8 8 d d b c c b d d 8 8 6 . 
+        . 6 8 d b c f . . f c b d 8 6 . 
+        . . . f c . . . . . . c f . . . 
+        . . . . f . . . . . . f . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(playerSprite)
+    playerSprite.setStayInScreen(true)
+    info.setScore(0)
+    info.setLife(3)
+    currentPlayerLevel = 1
+    currentDifficultyLevel = 1
+}
+let currentDifficultyLevel = 0
+let currentPlayerLevel = 0
+let playerSprite: Sprite = null
+effects.starField.startScreenEffect()
+let fireRateUpgradeList = [500, 300, 100]
+PlayerInit()
